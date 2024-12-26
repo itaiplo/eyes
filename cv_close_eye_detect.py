363df439c682a8e4d8fc25b98871ee4e52ee4c65
itaiplo <itaiplo@gmail.com>
2024-12-26 12:34:48 +0200

    works fine, before cleaning prints and init delay

--- cv_close_eye_detect.py
+++ cv_close_eye_detect.py
@@ -3,7 +3,7 @@ import time
 import top_cv
 import keyboard
 
-def main_fanc(freq_control=0.1,mode=""):
+def main_fanc(mode="",delay=0,sensitivity=0.5):
     # Paths to Haar cascades
     eye_cascPath = './haarcascade_eye_tree_eyeglasses.xml'  # Eye detection model
     face_cascPath = './haarcascade_frontalface_alt.xml'     # Face detection model
@@ -14,30 +14,32 @@ def main_fanc(freq_control=0.1,mode=""):
     # Open the video capture
     cap = cv2.VideoCapture(0)
     counter=0
+    counter_hit=0
     start = time.time()
-    # for i in range(2):
-    #     if i==0:
-    #         ret=input("print y when ready to start setup open eyes: ")
-    #     if (ret=="y"):
-    while True:
-        if(mode!="init_setup"):
-            if keyboard.is_pressed('r'):
-                inp=input("input was detectet, choose what you want to do(0 for beginigng reset, 1 for ignore reset, 2 for exit)")
-                if(inp==2):
-                    exit()
-                if(mode==""):
-                    if(inp==0):
-                        break
-                    if(inp==1):
-                        counter_magic, time_magic=0 #TODO reset counters and timer of thresh hold to zero
-            if(mode=="setup"):
-                if((end-start)>30):
-                    return end-start, counter
-                
+    end=start
 
-            
-        counter=counter+1
+   
+    while True:
+        if mode == 'run':
+            if end-start>15:
+                if counter_hit/counter>float( sensitivity):
+                    print("im here")
+                    return 1
+                else:
+                    counter_hit=0
+                    counter=0
+                    start = time.time()
+                    end=start
+                    print("im here...............starting again")
+                
+        if mode == 'setup_open' or mode == 'setup_closed':
+            if end-start>15:
+                if (counter_hit/counter>0.8):
+                    return 1
+                else:
+                    return 0
         ret, img = cap.read()
+        counter=counter+1
         if ret:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert frame to grayscale
 
@@ -66,8 +68,12 @@ def main_fanc(freq_control=0.1,mode=""):
                     )
 
                     if len(eyes) == 0:
+                        if mode =='setup_closed':
+                            counter_hit=counter_hit+1
                         print('The eyes are closed')
                     else:
+                        if mode =='setup_open' or mode=='run':
+                            counter_hit=counter_hit+1
                         print('The eyes are open')
 
             else:
@@ -76,15 +82,19 @@ def main_fanc(freq_control=0.1,mode=""):
             # Resize the frame for display
             resized_frame = cv2.resize(img, (400, 400), interpolation=cv2.INTER_LINEAR)
             cv2.imshow('Video Feed', resized_frame)
+        
 
         # Break the loop on 'q' or 'Q'
         key = cv2.waitKey(1)
         if key == ord('q') or key == ord('Q'):
             break
-        time.sleep(freq_control)
-        end = time.time()
         print ("counter is"+ str(counter))
+        end = time.time()
         print("time is" +str(end - start))
+        if mode=="init_setup":
+            if(end - start>15):
+                return (end - start,counter)
+        time.sleep(delay)
 
     # Release the capture and close all OpenCV windows
     cap.release()
--- top_cv.py
+++ top_cv.py
@@ -1,44 +1,58 @@
 import cv_close_eye_detect
 import keyboard
 import time
+global_delay=0
 
 def init_setup():
     time, laps=cv_close_eye_detect.main_fanc(mode="init_setup")
-    base_freq=laps/time
-    delay = max(0, 0.1 - (1 / base_freq))
-    if
+    base_freq = laps / time
+    global global_delay
+    global_delay = max(0, 0.1 - (1 / base_freq))
+    global_delay=0 # remove if want to use the delay
+    print("delay is: ", global_delay)
+    
+
 def open_eyes_setup():
     while (1):
         ret=input("print y when ready to start setup open eyes: ")
         if (ret=="y"):
-            ret_func=cv_close_eye_detect.main_fanc(mode="setup_open")
+            global global_delay
+            ret_func=cv_close_eye_detect.main_fanc(mode="setup_open",delay=global_delay)
             if (ret_func==1):
                 print("setup open eyes secceded")
                 break
+            else:
+                print("setup open eyes failed start again")
+                
  
 
 def closed_eyes_setup():
     while (1):
         ret=input("print y when ready to start setup closed eyes: ")
         if (ret=="y"):
-            ret_func=cv_close_eye_detect.main_fanc(mode="setup_closed")
+            ret_func=cv_close_eye_detect.main_fanc(mode="setup_closed",delay=global_delay)
             if (ret_func==1):
                 print("setup closed eyes secceded")
                 break
+            else:
+                print("setup closed eyes failed start again")
+                
 
 def setup_open_and_close():
     open_eyes_setup()
     closed_eyes_setup()
     print ("setup closed and open done")
 
-def handle_reset():
-    inp=input("input was detectet, choose what you want to do(0 for beginigng reset, 1 for ignore reset, 2 for exit)")
-    if(inp==0):
-        print("starting setup again")
-        break
-        setup_open_and_close()
-    if(inp==1):
-        //we stoped here.. need to write better the hendle reset. look at line 23 of hendle reset during setup procces
+def run_proccess():
+    sensitivity=input("enter sensitivity value: ")
+    print ("proccess started")
+    cv_close_eye_detect.main_fanc(mode="run",delay=global_delay,sensitivity=sensitivity)
 
+def main():
+    # init_setup()
+    # print("start setup")    
+    # setup_open_and_close()
+    run_proccess()
 
-
+if __name__ == "__main__":
+    main()
